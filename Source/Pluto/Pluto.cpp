b788e91e46a9fad82eb0f6592a93568692badfbb
Ahmad El Okla <elokla14@gmail.com>
2023-04-09 20:02:24 -0600

    Initial commit

---
+++ Source/Pluto/Pluto.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Pluto.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Pluto, "Pluto" );
